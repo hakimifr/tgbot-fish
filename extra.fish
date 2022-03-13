f8e5ada6c7b87a1a3abb22155202153a1e7f862c
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-13 13:02:43 +0800

    Create tgbot.fish
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- extra.fish
+++ extra.fish
@@ -19,3 +19,18 @@ set -g saving_group_id "-1001607510711"
 set -g tagger_group_id "-1001530403261"
 set -g bot_owner_id "1024853832"
 set -g rm6785_update_sticker "CAACAgUAAxkBAAED_CFiFIVi0Z1YX3MOK9xnaylscRhWbQACNwIAAt6sOFUrmjW-3D3-2yME"
+set -g help_message '`.calc` \-\> Do math calculations
+`.magisk` \-\> Get latest magisk stable and canary
+`.fwdpost` \-\> \(deprecated\) Forward post to @RM6785 \(this command is restricted to testing group\)
+`.postupdatesticker` \(deprecated\) \-\> Post update sticker to @RM6785 \(this command is restricted to testing group\)
+`.sticker` \-\> Post update sticker to @RM6785
+`.post` \-\> Post ROM/recovery to update channel, syntax: `\.post \<replying to a message\>`
+`.save` \(bot owner only\) \-\> Save message
+`.log` \(bot owner only\) \-\> Take logs, available type: all, normal, radio
+`.ofp` \-\> Get ofp for a device, syntax: `.ofp RMX6969 C.69 IN`
+`.stat` \-\> View stat
+`.uptime` \-\> View this laptop uptime
+`.restart` \(bot owner only\) \-\> Restart the bot
+`.reload` \(bot owner only\) \-\> Reload all modules
+`.load` \(bot owner only\) \-\> Load a module, syntax: `\.load modules/gay\.sh`
+`.unload` \(bot owner only\) \-\> Unload a module, syntax: `\.unload modules/gay\.sh`'
---
+++ tgbot.fish
@@ -0,0 +1,89 @@
+#!/bin/fish
+
+source extra.fish
+source util.fish
+source modules_loader.fish
+
+update_init
+while true
+    update
+    set -ge ret_lowered_msg_text
+    set -g ret_lowered_msg_text (string lower "$ret_msg_text")
+    test -n "$ret_lowered_msg_text" && echo "Text received: $ret_lowered_msg_text"
+
+    # Run modules
+    run_modules
+
+    switch $ret_lowered_msg_text
+        case '/help*' '.help*'
+            begin
+                tg --replymarkdownv2msg "$ret_chat_id" "$ret_msg_id" "$help_message" # $help_message located on extra.fish
+            end &
+        case '/test*' '.test*'
+            begin
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "bot is running"
+            end &
+        case '.calc*'
+            begin
+                set -l trimmed (string replace -r '^.calc' '' $ret_lowered_msg_text)
+                set -l calced (echo $trimmed | bc -l 2>&1)
+                if not echo "$calced" | grep -qi 'syntax error'
+                    tg --replymsg "$ret_chat_id" "$ret_msg_id" "$calced"
+                else
+                    tg --replymsg "$ret_chat_id" "$ret_msg_id" "Error occured"
+                end
+            end &
+        case '.magisk*'
+            begin
+                tg --sendmsg "$RET_CHAT_ID" "Fetching latest Magisk stable"
+                set -l latest (
+                    curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest |
+                        grep "Magisk-v**.*.apk" |
+                        cut -d : -f 2,3 |
+                        tr -d \" |
+                        cut -d, -f2 |
+                        tr -d '\n' |
+                        tr -d ' '
+                )
+                set -l canary "https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk"
+                tg --editmarkdownv2msg "$RET_CHAT_ID" "$SENT_MSG_ID" "[Latest stable]($LATEST_STABLE)"
+            end &
+        case '.neofetch'
+            begin
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "This may take a while as there's quite a few packages in this laptop..."
+                set -l neofetch_output (neofetch --stdout)
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "$neofetch_output"
+            end &
+        case '.save'
+            if not is_botowner
+                err_not_botowner
+                set -ge ret_lowered_msg_text ret_msg_text
+                continue
+            else if test "$ret_replied_msg_id" = null
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Reply to a message to save thx"
+                set -ge ret_lowered_msg_text ret_msg_text
+                continue
+            end
+            tg --replymsg "$ret_chat_id" "$ret_msg_id" "Please wait..."
+            tg --forwardmsg "$ret_chat_id" "$saving_group_id" "$ret_msg_id"
+            tg --editmsg "$ret_chat_id" "$ret_msg_id" "Message forwarded"
+        case '*@hakimi0804*'
+            begin
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Saving this message link so Hakimi can read it later..."
+                set -l reply_msg_id $sent_msg_id
+                set -l group_id (string replace '^-100' '' $ret_chat_id)
+                tg --sendmsg "$tagger_group_id" "New tag: https://t.me/c/$group_id/$ret_msg_id"
+                tg --delmsg "$ret_chat_id" "$reply_msg_id"
+            end &
+        case '/gay*'
+            begin
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Determining your gayness, please wait..."
+                set -l level (shuf -i 0-165 -n1)
+                while test "$level" -gt 100
+                    set level (shuf -i 0-165 -n1)
+                end
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "You are $level% gay"
+            end &
+    end
+    set -ge ret_lowered_msg_text ret_msg_text
+end
