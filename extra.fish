d23732028b95c0a51616f423f9917441378afb69
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-13 13:03:04 +0800

    Convert indentation
    * 4 hard tab -> 4 soft tab (spaces)
    
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- extra.fish
+++ extra.fish
@@ -2,18 +2,18 @@
 
 
 function round -d "round a number"
-	if test (count $argv) -ne 2
-		echo "Invalid amount of arguements!" >&2
-		return 1
-	end
-	set -l float $argv[1]
-	set -l decimal_point $argv[2]
-	printf "%.{$decimal_point}f" $float
+    if test (count $argv) -ne 2
+        echo "Invalid amount of arguements!" >&2
+        return 1
+    end
+    set -l float $argv[1]
+    set -l decimal_point $argv[2]
+    printf "%.{$decimal_point}f" $float
 end
 
 set -g fwd_approved_chat_id \
-	"-1001299514785" \
-	"-1001155763792"
+    "-1001299514785" \
+    "-1001155763792"
 set -g fwd_to "-1001384382397"
 set -g saving_group_id "-1001607510711"
 set -g tagger_group_id "-1001530403261"
