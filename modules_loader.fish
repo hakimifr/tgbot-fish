878007ed981c6f8fa4fb9c55a9c1ec2dec4f9689
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-14 12:45:19 +0800

    modules_loader: Redesign
    * Take advantage of fish's event emitter
    
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules_loader.fish
+++ modules_loader.fish
@@ -1,40 +1,47 @@
 #!/bin/fish
 
-function load_modules -d "Load all modules"
-    set -g modules (find modules -maxdepth 1 -iname '*.fish')
-    # An array of modules to be excluded,
-    # useful for debugging or otherwise modules
-    # that's only need to be loaded at a certain time
-    set -g modules_exclude \
-            "modules/hakimi_afk.fish" \
-            "modules/shell.fish"
+function load_modules
+    set -g exclude_modules \
+        "modules/hakimi_afk.fish"
 
-    set -e loaded_modules
+    set -ge loaded_modules
+    set -ge modules_events
+
+    pr_info "modules_loader" "Searching for modules"
+    set -g modules (find modules -maxdepth 1 -iname '*.fish')
+
+    pr_info "modules_loader" "Starting to load modules"
     for module in $modules
-        pr_info modules_loader "Loading module: $module"
-        for exclude_module in $modules_exclude
-            if test "$module" = "$exclude_module"
-                pr_warn "Skipping module: $module"
-                set -g skip_iter true
+        set -l exclude_module_continue_marker false
+        for excl in $exclude_modules
+            if test "$module" = "$excl"
+                set exclude_module_continue_marker true
                 break
             end
         end
-
-        if test "$skip_iter" = true
-            set -e skip_iter
+        if test "$exclude_module_continnue_marker" = true
+            pr_info "modules_loader" "Skipping module: $module"
+            set exclude_module_continue_marker false
             continue
         end
 
-        source "$module"
-        set -g loaded_modules $loaded_modules $module
+        pr_info "modules_loader" "Loading module: $module"
+        set -l old_modules_event $modules_events
+        source $module
+        if test "$old_modules_event" = "$modules_events"
+            pr_warn "modules_loader" "Module $module does not append anything to \$modules_events"
+            pr_error "modules_loader" "Failed to load module $module!"
+            continue
+        end
+
+        set -ga loaded_modules $module
+        pr_info "modules_loader" "Loaded: $module"
     end
 end
 
-function run_modules -d "Run all modules"
-    for module in $loaded_modules
-        set -l function_name (basename $module)
-        set -l function_name (string replace -r '.fish$' '' $function_name)
-        $function_name
+function run_modules
+    for event in $modules_events
+        emit $event
     end
 end
 
