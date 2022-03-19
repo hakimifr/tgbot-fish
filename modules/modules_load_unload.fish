4274665cac1668c7b151c0c732cae34b146b0c41
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-19 11:14:20 +0800

    modules: modules_load_unload: Adapt to new modules loader implementation
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/modules_load_unload.fish
+++ modules/modules_load_unload.fish
@@ -1,7 +1,12 @@
 #!/bin/fish
 
-set -ga modules_events "modules_management"
-function modules_load_unload -d "Module: modules/modules_load_unload" --on-event 'modules_management'
+set -g __module_name "Modules load and unload"
+set -g __module_description "To be used to load and unload modules from telegram"
+set -g __module_version 69
+set -g __module_events "tgmodulemanager"
+set -g __module_functions "modules_load_unload"
+
+function modules_load_unload -d "Module: modules/modules_load_unload" --on-event 'tgmodulemanager'
     switch $ret_lowered_msg_text
         case '.unload*'
             if not is_botowner
@@ -9,97 +14,41 @@ function modules_load_unload -d "Module: modules/modules_load_unload" --on-event
                 return
             end
 
-            set -l module_name (string replace -r '^.unload ' '' $ret_msg_text)
-            set -l module_exist false
-
-            # Do not even waste our time if module is not given
-            if test -z (string replace -r '^.unload' '' $ret_msg_text)
-                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Please give a module to load/unload thanks"
+            if test -z (string replace -r '^.unload' $ret_lowered_msg_text)
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Give module name to unload please"
                 return
             end
 
-            tg --replymsg "$ret_chat_id" "$ret_msg_id" "Please wait"
-            # Check if it even exists
-            set -l module_fname_index 1
-            for module in $loaded_modules
-                if test "$module_name" = "$module"
-                    # If so erase it
-                    pr_debug "modules_load_unload" "Erasing $module_name from \$loaded_modules"
-                    set -ge loaded_modules[$module_fname_index]
-                    set module_exist true
-                    break
-                end
-                set module_fname_index (math $module_fname_index + 1)
+            set -l module_trimmed (string replace -r '^.unload ' '' $ret_lowered_msg_text)
+            tg --replymsg "$ret_chat_id" "$ret_msg_id" "Unloading $module_trimmed"
+            __module_unload $module_trimmed
+            if test $status -eq 2
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to unload $module_trimmed, it does not exist."
+            else
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to unload $module_trimmed, status: $status"
             end
-            if test "$module_exist" = false
-                pr_error "modules_load_unload" "Module $module_name does not exist."
-                tg --editmsg "$ret_chat_id" "$sent_msg_id" "That module does not exist."
-                return
-            end
-
-            # Find out which index the module's event is on
-            set -l module_index 1
-            for module in $loaded_modules
-                if test "$module" = "$module_name"
-                    break
-                end
-                set module_index (math $module_index + 1)
-            end
-
-            # Erase it
-            set -l event_index 1
-            for event in $modules_events
-                if test "$event_index" -eq "$module_index"
-                    pr_debug "modules_load_unload" "Erasing module $module_name's event ($modules_events[$event_index]) from \$event_index"
-                    set -ge modules_events[$event_index]
-                    break
-                end
-                set event_index (math $event_index + 1)
-            end
-            pr_info "modules_load_unload" "Unloaded module: $module_name"
-            tg --editmsg "$ret_chat_id" "$sent_msg_id" "Module $module_name unloaded"
         case '.load*'
             if not is_botowner
                 err_not_botowner
                 return
             end
 
-            set -l module_name (string replace -r '^.load ' '' $ret_msg_text)
-
-            # Do not even waste our time if module is not given
-            if test -z (string replace -r '^.load' '' $ret_msg_text)
-                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Please give a module to load/unload thanks"
+            if test -z (string replace -r '^.load' $ret_lowered_msg_text)
+                tg --replymsg "$ret_chat_id" "$ret_msg_id" "Give module name to load please"
                 return
             end
 
-            tg --replymsg "$ret_chat_id" "$ret_msg_id" "Please wait..."
-            if not test -f $module_name
-                pr_warn "modules_load_unload" "Cannot find $module_name!"
-                pr_error "modules_load_unload" "Failed to load $module_name."
-                tg --editmsg "$ret_chat_id" "$sent_msg_id" "That module does not exist."
-                return
+            set -l module_trimmed (string replace -r '^.load ' '' $ret_lowered_msg_text)
+            tg --replymsg "$ret_chat_id" "$ret_msg_id" "Loading module $module_trimmed"
+            __module_load $module_trimmed
+            if test $status -eq 2
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to load $module_trimmed, file does not exist."
+            else if test $status -eq 3
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to unload $module_trimmed, one or more property isn't set by that module."
+            else if test $status -eq 4
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to unload $module_trimmed, fatal: conflicting events/functions with other module."
+            else
+                tg --editmsg "$ret_chat_id" "$sent_msg_id" "Failed to unload $module_trimmed, status: $status"
             end
-
-            # Make sure it's not already loaded
-            for module in $loaded_modules
-                if test "$module" = "$module_name"
-                    pr_warn "modules_load_unload" "Module $module is already loaded."
-                    tg --editmsg "$ret_chat_id" "$sent_msg_id" "That module is already loaded."
-                    return
-                end
-            end
-
-            pr_info "modules_load_unload" "Loading module: $module_name"
-            set -l old_modules_event $modules_events
-            source $module_name
-            if test "$old_modules_event" = "$modules_events"
-                pr_warn "modules_load_unload" "Module $module_name does not append anything to \$modules_events"
-                pr_error "modules_load_unload" "Failed to load $module_name"
-                return
-            end
-            pr_info "modules_load_unload" "Loaded: $module_name"
-
-            set -ga loaded_modules "$module_name"
-            tg --editmsg "$ret_chat_id" "$sent_msg_id" "Module $module_name loaded"
     end
 end
