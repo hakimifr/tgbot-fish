133e53e305d2e3f5e741126073fbd0b6ae7c2920
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-20 01:26:51 +0800

    modules: afk: Fixup
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/hakimi_afk.fish
+++ modules/hakimi_afk.fish
@@ -9,9 +9,9 @@ set -g __module_functions "hakimi_afk"
 function hakimi_afk -d "Hakimi's AFK module" --on-event 'afk'
     switch $ret_lowered_msg_text
         case '*@hakimi0804*'
-            tg --replymarkdownv2msg "$ret_chat_id" "$ret_msg_id" "AFK MODULE: Hakimi is afk \\(Most probably sleeping\\)
-_This message will auto-delete in 2 sec_"
-            sleep 2
-            tg --delmsg "$ret_chat_id" "$ret_msg_id"
+            tg --replymarkdownv2msg "$ret_chat_id" "$ret_msg_id" "AFK MODULE: Hakimi is afk \\(Most probably sleeping\\)\\. View details of this module with `\\.modinfo hakimi_afk.fish`
+_This message will auto\\-delete in 3 sec_"
+            sleep 3
+            tg --delmsg "$ret_chat_id" "$sent_msg_id"
     end
 end
