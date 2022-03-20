1ef9684cb18f39b8178cea23e2ac69cba3c0decd
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-20 13:07:58 +0800

    modules: afk: Add message to list all modules
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/hakimi_afk.fish
+++ modules/hakimi_afk.fish
@@ -9,7 +9,7 @@ set -g __module_functions hakimi_afk
 function hakimi_afk -d "Hakimi's AFK module" --on-event afk
     switch $ret_lowered_msg_text
         case '*@hakimi0804*'
-            tg --replymarkdownv2msg "$ret_chat_id" "$ret_msg_id" "AFK MODULE: Hakimi is afk \\(Most probably sleeping\\)\\. View details of this module with `\\.modinfo hakimi_afk.fish`
+            tg --replymarkdownv2msg "$ret_chat_id" "$ret_msg_id" "AFK MODULE: Hakimi is afk \\(Most probably sleeping\\)\\. View details of this module with `\\.modinfo hakimi_afk.fish`\\. List all loaded module with `\\.lsmod`\\.
 _This message will auto\\-delete in 3 sec_"
             sleep 3
             tg --delmsg "$ret_chat_id" "$sent_msg_id"
