dc416788c0a24f2a39c16b81b03966ac6498ba0f
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-04-01 02:44:34 +0800

    modules/group_management: Finish TODOs chore
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/group_management.fish
+++ modules/group_management.fish
@@ -39,14 +39,85 @@ function purge --on-event modules_trigger
             set -l purge_end_time (date +%s.%N)
             set -l purge_diff_time (math $purge_end_time - $purge_start_time)
             tg --sendmsg $ret_chat_id "Purge completed. Took $(round $purge_diff_time 3)"
+        case '.pin'
+            verify
+            or return
+
+            tg --pinmsg $ret_chat_id $ret_replied_msg_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Message pinned."
+
+        case '.unpin'
+            verify
+            or return
+
+            tg --unpinmsg $ret_chat_id $ret_replied_msg_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Message unpinned."
+
+        case '.ban'
+            verify
+            or return
+
+            tg --ban $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Banned that user."
+
+        case '.unban'
+            verify
+            or return
+
+            tg --unban $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Unbanned that user."
+
+        case '.kick'
+            verify
+            or return
+
+            tg --ban $ret_chat_id $ret_replied_msgger_id
+            tg --unban $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Kicked that user."
+
+        case '.mute'
+            verify
+            or return
+
+            tg --mute $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Muted that user."
+
+        case '.unmute'
+            verify
+            or return
+
+            tg --unmute $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "User unmuted."
+
+        case '.promote'
+            verify
+            or return
+
+            tg --promote $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Promoted that user."
+
+        case '.demote'
+            verify
+            or return
+
+            tg --demote $ret_chat_id $ret_replied_msgger_id
+            tg --editmsg $ret_chat_id $sent_msg_id "Demoted that user."
     end
+end
+
+function verify
+    tg --replymsg $ret_chat_id $ret_msg_id "Verifying"
+    test $ret_replied_msg_id != null
+    or tg --editmsg $ret_chat_id $sent_msg_id "Reply to a message please" && return 1
+
+    is_admin $ret_chat_id $msgger
+    or tg --editmsg $ret_chat_id $sent_msg_id "Error, you are not an admin" && return 1
+
+    is_admin $ret_chat_id $this_bot_id
+    or tg --editmsg $ret_chat_id $sent_msg_id "Error, I am not admin" && return 1
+
+    return 0
 end
 
 # Init
 set -g this_bot_id (curl -s $API/getMe | jq .result.id)
-
-
-## Chore
-# - ban
-# - mute
-# - kick
