19e92a4c886ba8accd520dad07fa1ef1a43fefed
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-11-11 07:40:27 +0800

    modules/fun: Cheating
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/fun.fish
+++ modules/fun.fish
@@ -8,6 +8,13 @@ set -g __module_help_message "Bored? Try this module\!
 `/gay` \-\> Determine your gayness\.
 `/sexy`, `.sexy` \-\> Determine your sexiness\."
 
+set -g cheat_user
+set -a cheat_user 1554437068 # miko
+set -a cheat_user 1655514932 # adib
+set -a cheat_user 1084530895 # aqil
+
+pr_debug fun "cheat users: $cheat_user"
+
 function telegram --on-event modules_trigger
     switch $ret_lowered_msg_text
         case '/gay*'
@@ -16,13 +23,23 @@ function telegram --on-event modules_trigger
             while test $level -gt 100
                 set level (shuf -i 0-165 -n1)
             end
-            tg --editmsg $ret_chat_id $sent_msg_id "You are $level% gay"
+
+            if contains -- $msgger $cheat_user
+                tg --editmsg $ret_chat_id $sent_msg_id "You are 200% gay"
+            else
+                tg --editmsg $ret_chat_id $sent_msg_id "You are $level% gay"
+            end
         case '/sexy*' '.sexy'
             tg --replymsg $ret_chat_id $ret_msg_id "Determining your sexiness, please wait..."
             set -l sexiness (shuf -i 0-165 -n1)
             while test $sexiness -gt 100
                 set sexiness (shuf -i 0-165 -n1)
             end
-            tg --editmsg $ret_chat_id $sent_msg_id "You are $sexiness% sexy"
+
+            if contains -- $msgger $cheat_user
+                tg --editmsg $ret_chat_id $sent_msg_id "You are -200% sexy"
+            else
+                tg --editmsg $ret_chat_id $sent_msg_id "You are $sexiness% sexy"
+            end
     end
 end
